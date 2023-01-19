a97a0bd03ebbe51d1f5fae1ae7ccc8045e2d670f
my <my@AIWS-PC>
2023-01-19 12:46:23 +0530

    commit

---
+++ my-calculator/src/test/java/com/smdimran/mycalculator/MyCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smdimran.mycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
